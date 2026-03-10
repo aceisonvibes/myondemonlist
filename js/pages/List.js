217495f98463de635f1a3a0e24de929c9cb677a3
aceisonvibes <aaronfigueroadj@gmail.com>
2026-03-10 17:12:48 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -24,14 +24,40 @@ export default {
             <div class="list-container">
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
+                        <!-- Rank number -->
                         <td class="rank">
-                            <p v-if="i + 1 <= 75" class="level-pos-top75">#{{ i + 1 }}</p>
-                            <p v-else-if="i + 1 <= 150" class="level-pos-76-150">#{{ i + 1 }}</p>
-                            <p v-else class="level-pos-151plus">Legacy</p>
+                            <p 
+                                v-if="i + 1 <= 150" 
+                                class="type-label-lg"
+                                :class="{
+                                    'level-pos-top75': i + 1 <= 75,
+                                    'level-pos-76-150': i + 1 > 75 && i + 1 <= 150,
+                                    'level-pos-151plus': i + 1 > 150
+                                }"
+                            >
+                                #{{ i + 1 }}
+                            </p>
+                            <p 
+                                v-else 
+                                class="type-label-lg level-pos-151plus"
+                            >
+                                Legacy
+                            </p>
                         </td>
+
+                        <!-- Level name -->
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
                             <button @click="selected = i">
-                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
+                                <span 
+                                    class="type-label-lg"
+                                    :class="{
+                                        'level-pos-top75': i + 1 <= 75,
+                                        'level-pos-76-150': i + 1 > 75 && i + 1 <= 150,
+                                        'level-pos-151plus': i + 1 > 150
+                                    }"
+                                >
+                                    {{ level?.name || \`Error (\${err}.json)\` }}
+                                </span>
                             </button>
                         </td>
                     </tr>
@@ -43,7 +69,6 @@ export default {
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
-
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points when completed</div>
@@ -58,12 +83,10 @@ export default {
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
                     </ul>
-
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
                     <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
                     <p v-else>This level does not accept new records.</p>
-
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
                             <td class="percent">
@@ -81,7 +104,6 @@ export default {
                         </tr>
                     </table>
                 </div>
-
                 <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
@@ -106,30 +128,14 @@ export default {
                         </ol>
                     </template>
                     <h3>Submission Requirements</h3>
-                    <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
-                    </p>
-                    <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
-                    </p>
-                    <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
-                    </p>
-                    <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
-                    </p>
-                    <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
-                    <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
-                    </p>
+                    <p>Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)</p>
+                    <p>Achieved the record on the level that is listed on the site - please check the level ID before you submit a record</p>
+                    <p>Have either source audio or clicks/taps in the video. Edited audio only does not count</p>
+                    <p>The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this</p>
+                    <p>The recording must also show the player hit the endwall, or the completion will be invalidated.</p>
+                    <p>Do not use secret routes or bug routes</p>
+                    <p>Do not use easy modes, only a record of the unmodified level qualifies</p>
+                    <p>Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level</p>
                 </div>
             </div>
         </main>
@@ -164,16 +170,16 @@ export default {
         this.editors = await fetchEditors();
 
         if (!this.list) {
-            this.errors = [
-                "Failed to load list. Retry in a few minutes or notify list staff.",
-            ];
+            this.errors = ["Failed to load list. Retry in a few minutes or notify list staff."];
         } else {
             this.errors.push(
                 ...this.list
                     .filter(([_, err]) => err)
                     .map(([_, err]) => `Failed to load level. (${err}.json)`)
             );
-            if (!this.editors) this.errors.push("Failed to load list editors.");
+            if (!this.editors) {
+                this.errors.push("Failed to load list editors.");
+            }
         }
 
         this.loading = false;
